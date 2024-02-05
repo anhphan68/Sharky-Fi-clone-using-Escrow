7c6b55eca8c2390ea3121585373a0158e2597e2e
anhphan68 <159033620+anhphan68@users.noreply.github.com>
2024-02-05 22:05:00 +0700

    declare lending borrowing program id

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("14UHtjssoVvXk3M4TkxcDxgPoJAAitdjgZ1XHD8fsfjj");
 
 #[program]
 pub mod nft_lend_borrow {
